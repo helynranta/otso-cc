bc55c911ac072ab797e53e87686c758aaf042d4f
lorpo <lorpo@gmail.com>
2015-12-07 15:04:19 +0200

    base of the datathings

--- server/app.js
+++ server/app.js
@@ -5,37 +5,73 @@ var random  = require('randomstring');
 
 var port = process.env.PORT || 3000;
 
+var accounts, orders, feedback = {}
+
 app.use(express.static(__dirname+"/htdocs/"));
 
-app.get('/postFeedback', function(req, res) {
-    console.log("got post");
-    
-    fs.readFile(__dirname + "/data/feedback.json", function (err, data) {
-        try {
-             data = JSON.parse(data);
-        } catch (err) {
-            console.log(err)
-        } finally {
-            var rand = "";
-            while((rand = random.generate(13)) != undefined) {}
-            data[rand] = {
-                "email": "example@test.com",
-                "date": new Date().toJSON(),
-                "stars": [1,2,1,2],
-                "comment":"kaikki oli paskaa paitsi kusi",
-                "recall":false
-            }   
-        }
-        res.end("lol");
-    });
+app.get('/getFeedback', function(req, res) {
+    res.send();
+    res.end();
+});
+app.get('/getOrders', function(req, res) {
+    var data = loadOrders();
+    res.end(data);
 });
 
-app.get('/', function(req, res) {
-    
+app.get('/test', function(req, res) {
+
 })
 
 var server = app.listen(port, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log("App running at http://%s:%s", host, port);
+
+    loadOrders();
+    loadFeedback();
+    loadAccounts();
 });
+
+// functions
+var loadOrders = function () {
+    var source = {};
+    fs.readFile(__dirname+"/data/order.json", function(err, data) {
+        if(err) throw err;
+        try{
+            var data = JSON.parse(data);
+        }catch (err){
+            throw err;
+        }finally {
+            source = data;
+        }
+        orders = source;
+    });
+};
+var loadFeedback = function () {
+    var source = {};
+    fs.readFile(__dirname + "/data/feedback.json", function (err, data) {
+        if(err) throw err;
+        try{
+            var data = JSON.parse(data);
+        }catch (err){
+            throw err;
+        }finally {
+            source = data;
+        }
+        feedback = source;
+    });
+};
+var loadAccounts = function() {
+    // load all accounts to memory (bubbelcum)
+    fs.readFile(__dirname+"/data/accounts.json", function(err, data) {
+        if(err) throw err;
+        try {
+            var data = JSON.parse(data);
+        }
+        catch(err) {
+            throw err;
+        } finally {
+            accounts = data;
+        }
+    });
+}
