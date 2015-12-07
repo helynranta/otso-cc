b3b33b6f734a4ec316511c282bdaf5912940da1f
Lassi <lassilaati@gmail.com>
2015-12-07 11:57:14 +0000

    progress

--- server/app.js
+++ server/app.js
@@ -1,6 +1,28 @@
 var express	= require('express');
 var app		= express();
+var fs      = require("fs");
+var random  = require('randomstring');
 
 var port = process.env.PORT || 3000;
 
-app.listen(port);
+app.post('/postFeedback', function(req, res) {
+    fs.readFile(__dirname + "/data/feedback.json", 'utf-8', function (err, data) {
+        data = JSON.parse(data);
+        
+        var rand = "";
+        while((rand = random.generate(13)) != undefined) {}
+        data[rand] = {
+            "email": "example@test.com",
+            "date": new Date().toJSON(),
+            "stars": [1,2,1,2],
+            "comment":"kaikki oli paskaa paitsi kusi",
+            "recall":false
+        }
+    });
+});
+
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("App running at http://%s:%s", host, port);
+});
