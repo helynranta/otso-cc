8bbc3bc1c7ea870bf65191088d32a56f30d4b7e1
Anna <krevetko@gmail.com>
2015-12-10 13:51:51 +0200

    dashboard stuff, not done

--- client/src/ManufacturerDashboard.jsx
+++ client/src/ManufacturerDashboard.jsx
@@ -8,8 +8,9 @@ var React = require('react'),
   require("../src/css/style.css");
 
 var CreateOrder = require('./CreateOrder.jsx'),
-	SubcontractorsScreen = require('./SubcontractorsScreen.jsx');
-
+	SubcontractorsScreen = require('./SubcontractorsScreen.jsx'),
+	DashboardComments = require('./DashboardComments.jsx'),
+	DashboardSubcontractors = require('./DashboardSubcontractors.jsx');
 
 var ManufacturerDashboard = React.createClass({
 	mixins: [Navigation],
@@ -25,9 +26,15 @@ var ManufacturerDashboard = React.createClass({
 	render: function() {
 		return(
 			<div>
-				<div className="menu-item">
+				<div className="text-center">
 					<h2>Dashboard</h2>
 				</div>
+				
+				<div id="container-comments" className="text-center"></div>
+
+				<div id="container-subcontractors" className="text-center"></div>
+				
+				{/*
 				<div className="menu-item">
 					<Button bsStyle="primary" onClick={this.navigateSubcontractors}>Subcontractors</Button>
 				</div>
@@ -37,12 +44,39 @@ var ManufacturerDashboard = React.createClass({
 				<div className="menu-item">
 					<Button bsStyle="primary" onClick={this.navigateOrder}>Create order</Button>
 				</div>
-				<div className="menu-item">newest comments, best subcontractors or weather here</div>
-                <div className="text-center">
-                    <img  className="menu-item text-center" src="http://i.imgur.com/XKjuVJn.png" width="500"/>
-                </div>
+				*/}			
 			</div>
 		);
+	},
+	componentDidMount: function() {
+		let sc_data,
+			fb_data,
+			ajaxes = [];
+
+		ajaxes[0] = Promise.resolve($.ajax({
+				url:'https://otso-cc-lasshi.c9users.io/subcontractors/rating/*',
+				contentType:'application/json',
+				dataType:'json',
+				type:'GET'
+			})
+		).then((data) => {
+			sc_data = data;
+		});
+
+		ajaxes[1] = Promise.resolve($.ajax({
+				url:'https://otso-cc-lasshi.c9users.io/feedback/',
+				contentType:'application/json',
+				dataType:'json',
+				type:'GET'
+			})
+		).then((data) => {
+			fb_data = data;
+		});
+
+		Promise.all(ajaxes).then(() => {
+			React.render(<DashboardComments data={fb_data} />, document.getElementById('container-comments'));
+			React.render(<DashboardSubcontractors data={sc_data} />, document.getElementById('container-subcontractors'));
+		});
 	}
 });
 
